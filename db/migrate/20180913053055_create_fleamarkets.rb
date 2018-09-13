a47aa3d545d6a4c46dc32a9d312fa396f481f358
sangyeol kim <twysg@likelion.org>
2018-09-13 15:30:49 +0000

    pub & fleamarket view

--- db/migrate/20180913053055_create_fleamarkets.rb
+++ db/migrate/20180913053055_create_fleamarkets.rb
@@ -3,6 +3,7 @@ class CreateFleamarkets < ActiveRecord::Migration[5.1]
     create_table :fleamarkets do |t|
       t.string :title
       t.text :content
+      t.text :modal
       t.string :image
 
       t.timestamps
