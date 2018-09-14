f68402f88429855f0b9a86e12022f405e257259d
PARK <kkami121@naver.com>
2018-09-14 20:35:47 +0000

    박귈 분실물 조져놈

--- db/schema.rb
+++ db/schema.rb
@@ -61,15 +61,6 @@ ActiveRecord::Schema.define(version: 20180913160311) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "pubs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.text "modal"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
