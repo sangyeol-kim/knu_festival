b50cc50559814896c28f3331e92b0551e67a1795
PARK <kkami121@naver.com>
2018-09-15 20:08:57 +0000

    modal update

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20180913160311) do
     t.integer "iconid"
     t.string "number"
     t.string "ps"
-    t.text "modal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
