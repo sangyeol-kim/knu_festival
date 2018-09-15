8c8a49121506a2090e1380a4fe41add7b752091e
totobiyo <chap95@likelion.org>
2018-09-15 19:09:56 +0000

    준혁

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  helper_method :mobile?
+  
+  private
+  def mobile? # has to be in here because it has access to "request"
+    request.user_agent =~ /\b(Android|iPhone|iPad|Windows Phone|Opera Mobi|Kindle|BackBerry|PlayBook)\b/i
+  end
 end
