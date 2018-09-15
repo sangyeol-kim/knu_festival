13d03ea10800daaf518413240029894d12cb5435
PARK <kkami121@naver.com>
2018-09-15 15:21:11 +0000

    귀열

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,11 @@ class CommentsController < ApplicationController
         redirect_back fallback_location: root_path
     end
     
+    
     def destroy
+        comment = Comment.find(params[:id])
+        comment.destroy
+        
+        redirect_back fallback_location: root_path
     end
 end
