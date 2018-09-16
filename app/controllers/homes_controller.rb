37dc3a338e5dea4c9eace20adb0c03009ffc559e
ChulMin Kim <kbs4674@likelion.org>
2018-09-16 08:15:32 +0000

    총 방문자수 시스템 기반 변경

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,18 +1,19 @@
 class HomesController < ApplicationController
-    before_action :log_impression, :only=> [:index], :validates_uniqueness_of => [:ip_address]
+    impressionist actions: [:index], unique: [:session_hash]
  
-    def log_impression
-        @visit_count = Home.find(1)
-        # this assumes you have a current_user method in your authentication system
-        @visit_count.impressions.create(ip_address: request.remote_ip)
-        @visit_count_total = Impression.all.count
-    end
+    #def log_impression
+        #@visit_count = Home.find(1)
+        ## this assumes you have a current_user method in your authentication system
+        #@visit_count.impressions.create(ip_address: request.remote_ip)
+        #@visit_count_total = Impression.all.count
+    #end
    
-    #def index
+    def index
+            @visit_count_total = Impression.all.count
     #    # 방문자수 체크
     #    # 반드시 rake db:seed 할 것.
     #    @visit_count = Home.find(1)
     #    abc = @visit_count.count_view += 1
     #    @visit_count.update_attribute "count_view", abc
-    #end
+    end
 end
