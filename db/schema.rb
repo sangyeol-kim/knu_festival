5c7f7e30ce6890fd5d5a46df49e17a471bca6812
ChulMin Kim <kbs4674@likelion.org>
2018-09-17 09:58:22 +0000

    password attribute 별도 추가

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180916130000) do
+ActiveRecord::Schema.define(version: 20180917095722) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20180916130000) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
   create_table "messages", force: :cascade do |t|
