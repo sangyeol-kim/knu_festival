6c10c7ea97fd1a931c4982a9928dfb14c9f7612d
ChulMin Kim <kbs4674@likelion.org>
2018-09-17 11:55:11 +0000

    공지사항 게시판 구현

--- app/controllers/secret_controller.rb
+++ app/controllers/secret_controller.rb
@@ -1,5 +1,6 @@
 class SecretController < ApplicationController
   def index
     @really_visit_count_total = Impression.all.count
+    @all_notices = AllNotice.order("created_at DESC").all
   end
 end
