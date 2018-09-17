be017f776794d68c60936f1840121e4aa9e417d9
HongSeokJun <tjrwns0529@gmail.com>
2018-09-17 00:26:53 +0000

    d

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180916130000) do
     t.integer "iconid"
     t.string "number"
     t.string "ps"
+    t.text "modal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
