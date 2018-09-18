6a73c92ec8a1ce039d64ea950cc5907ac94e782d
Hyeonseok seo <shsvkdldj@likelion.org>
2018-09-18 06:48:20 +0000

    노다지

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   helper_method :mobile?
-  
+  $visit_count = Home.find(1)
+        abc = $visit_count.count_view += 1
+        $visit_count.update_attribute "count_view", abc
+        
+        $really_visit_count_total = Impression.all.count
+        
+        $all_notices = AllNotice.order("created_at DESC").all
   private
   def mobile? # has to be in here because it has access to "request"
     request.user_agent =~ /\b(Android|iPhone|iPad|Windows Phone|Opera Mobi|Kindle|BackBerry|PlayBook)\b/i
