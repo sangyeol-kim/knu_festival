5663a86082f1cc0cda815d3081f9f1146927b63d
ChulMin Kim <kbs4674@likelion.org>
2018-09-21 16:05:41 +0000

    차트 height수치 수정

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -4,6 +4,6 @@ class ChartsController < ApplicationController
   end
   
   def new_users
-      render json: Impression.group(:referrer).count
+      render json: Impression.group(:referrer).where("referrer like ?", "%everytime%").count
   end
 end
