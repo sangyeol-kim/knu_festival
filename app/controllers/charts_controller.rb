d3f8868d5a9eb93ccd7bc12f49ced4e0493fe2ff
ChulMin Kim <kbs4674@likelion.org>
2018-09-21 16:18:02 +0000

    차트 controller 수정

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -4,7 +4,7 @@ class ChartsController < ApplicationController
   end
   
   def new_users
-      render json: Impression.group(:referrer).where("referrer like ?", "%facebook.com").or(Impression.group(:referrer).where("referrer like ?", "%everytime.kr")).count
+      render json: Impression.group(:referrer).where("referrer like ?", "%facebook.com%").or(Impression.group(:referrer).where("referrer like ?", "%everytime.kr%")).count
       #render json: Impression.group(:referrer).where("referrer like ?", "%everytime%").count
   end
 end
