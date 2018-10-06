8bfd73bf14e2e0392526b22ede8af04dad388258
ChulMin Kim <kbs4674@likelion.org>
2018-10-06 22:03:19 +0000

    오타 제거(schedule.rb)

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,8 +5,6 @@ def local(time)
         TZInfo::Timezone.get('Asia/Seoul').local_to_utc(Time.parse(time))
 end
  
-...
- 
 # 매 20분마다 코레일 API 시간 최신화
 every 1.minutes do
     rake "crawlling_weather:crawlling_weather"
